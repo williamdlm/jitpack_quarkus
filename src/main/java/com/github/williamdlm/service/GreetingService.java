9086e860ea8aab092314e7567ac743432267c75a
William Mota <william.mota.ext@spms.min-saude.pt>
2024-06-10 09:22:21 +0100

    Added camel.beans.xxx in to application.properties

--- src/main/java/com/github/williamdlm/service/GreetingService.java
+++ src/main/java/com/github/williamdlm/service/GreetingService.java
@@ -1,9 +1,9 @@
 package com.github.williamdlm.service;
 
 
-import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebService;
+
 @WebService
 public interface GreetingService {
     @WebMethod
